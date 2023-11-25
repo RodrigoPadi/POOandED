26b9f4d71de35cd2f6e2df4cdae07991fd034dac
Rodrigo Padilha <151454973+rodrigoPadi@users.noreply.github.com>
2023-11-25 11:41:54 -0300

    Update Videogame.py

--- videogameSimples-TESTE/Videogame.py
+++ videogameSimples-TESTE/Videogame.py
@@ -4,22 +4,22 @@ class VideogameException(Exception):
     def __init__(self, msg):
         super().__init__(msg)
 
-class Videogame():
 
-    def __init__(self, marca, dataFabricacao, modelo, numSerie):
-        self.__dataFabricacao = dataFabricacao
+class Videogame():
+    def __init__(self, marca):
+        self.__dataFabricacao = ''
         self.__marca = marca
-        self.__modelo = modelo
-        self.__numSerie = numSerie + '-' + str(date.today())
+        self.__modelo = ''
+        self.__numSerie = ''
 
     @property
     def dataFabricacao(self):
         return self.__dataFabricacao
-        
+
     @dataFabricacao.setter
     def dataFabricacao(self, novaData):
-        self.__dataFabricacao = novaData
-        
+        self.__dataFabricacao += novaData
+
     @property
     def marca(self):
         return self.__marca
@@ -27,22 +27,22 @@ class Videogame():
     @marca.setter
     def marca(self, novaMarca):
         self.__marca = novaMarca
-        
+
     @property
     def modelo(self):
         return self.__modelo
-        
+
     @modelo.setter
     def modelo(self, novoModelo):
-        self.__modelo = novoModelo
-        
+        self.__modelo += novoModelo
+
     @property
-    def numSerie (self):
+    def numSerie(self):
         return self.__numSerie
-        
+
     @numSerie.setter
     def numSerie(self, novoNumSerie):
-        self.__numSerie = novoNumSerie + '-' + str(date.today())
-        
+        self.__numSerie += novoNumSerie + '-' + str(date.today())
+
     def __str__(self):
         return f'Videogame: {self.__modelo}/ Data de fabricação: {self.__dataFabricacao}/ Marca: {self.__marca}/ Número de série: {self.__numSerie}'
