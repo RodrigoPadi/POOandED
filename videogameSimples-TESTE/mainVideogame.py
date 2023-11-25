12360c7223f05337776bf9b056ceec64af9f4d4c
Rodrigo Padilha <151454973+rodrigoPadi@users.noreply.github.com>
2023-11-25 11:44:05 -0300

    Update mainVideogame.py

--- videogameSimples-TESTE/mainVideogame.py
+++ videogameSimples-TESTE/mainVideogame.py
@@ -1,6 +1,9 @@
 from Videogame import Videogame, VideogameException
 
-v1 = Videogame('Playstation', '2023', 'PS5', '211')
+v1 = Videogame('Play')
+v1.dataFabricacao = '2023'
+v1.modelo = 'PS5'
+v1.numSerie = '211'
 print(v1)
-
+print(v1.__dict__)
 # EM CONSTRUÇÃO
